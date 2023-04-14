6ffa0d4e8e3cca53d17c1f0237476790945e9f94
Master X <117908721+MasterX-0@users.noreply.github.com>
2023-04-14 12:07:34 +0530

    Shell Scripting 🎶❤️

---
+++ 1st_Example.sh
@@ -0,0 +1,14 @@
+
+echo ""
+echo "Enter Your First Name : \c"
+read x
+
+echo ""
+echo "Enter Your 2nd Name : \c"
+read y
+
+echo ""
+echo "Your Full Name is $x $y"
+echo ""
+
+# Output : https://telegra.ph/file/155a250f2b14588ca5b31.jpg
